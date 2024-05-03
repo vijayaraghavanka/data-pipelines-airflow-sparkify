0a85937d036be7d4e694788e06d2f265cad4c7b5
vijayaraghavanka <165424511+vijayaraghavanka@users.noreply.github.com>
2024-05-03 01:34:55 -0400

    Update load_fact.py

--- plugins/operators/load_fact.py
+++ plugins/operators/load_fact.py
@@ -2,21 +2,25 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class LoadFactOperator(BaseOperator):
 
+
+class LoadFactOperator(BaseOperator):
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id,
+                 table_name,
+                 query,
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.table_name=table_name
+        self.query=query
+        
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        redshift_hook.run(f"insert into {self.table_name} {self.query}")
+        self.log.info(f'{self.table_name} Fact table loading is completed')
