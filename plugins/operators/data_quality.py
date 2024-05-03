0646de4b2e834ad8b7c8dc262319e1e0d91fb65e
vijayaraghavanka <165424511+vijayaraghavanka@users.noreply.github.com>
2024-05-03 01:34:21 -0400

    Update data_quality.py

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -8,15 +8,23 @@ class DataQualityOperator(BaseOperator):
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id,
+                 table_name,
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.table_name=table_name
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        self.log.info('Data Quality Check started')
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        for table in self.table_name:
+            records=redshift_hook.get_records(f"select count(*) from {table};")
+            if len(records) < 1 or len(records[0]) < 1:
+                raise ValueError(f"{table} table has no results")
+            if records[0][0] < 1:
+                raise ValueError(f"{table} table has no rows")
+            self.log.info(f"Data quality check for {table} table passed with {records[0][0]} records")
+
+            
