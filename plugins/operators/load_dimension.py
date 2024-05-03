c92b153b72506b7e7606c205d7f89dca773fdf29
vijayaraghavanka <165424511+vijayaraghavanka@users.noreply.github.com>
2024-05-03 01:34:37 -0400

    Update load_dimension.py

--- plugins/operators/load_dimension.py
+++ plugins/operators/load_dimension.py
@@ -3,20 +3,29 @@ from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
-
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id,
+                 table_name,
+                 truncate,
+                 query,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id=redshift_conn_id
+        self.table_name=table_name
+        self.truncate=truncate
+        self.query=query
+        
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        self.log.info(f"{self.table_name} - Dimension table loading started")
+        redshift_hook = PostgresHook(self.redshift_conn_id)
+        if self.truncate:
+            self.log.info(f"Clearing data from {self.table_name} table")
+            redshift_hook.run(f"Truncate table {self.table_name}")
+
+        redshift_hook.run(f"insert into {self.table_name} {self.query}")
+        self.log.info(f"{self.table_name} Dimension table loading is completed")
